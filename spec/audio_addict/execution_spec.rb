0c269e45c802d9fda986b2601d55b44c1c0a4d0b
Danny Ben Shitrit <db@dannyben.com>
2018-11-30 12:18:40 +0000

    attempt travis mock server integration

--- spec/audio_addict/execution_spec.rb
+++ spec/audio_addict/execution_spec.rb
@@ -3,22 +3,10 @@ require 'spec_helper'
 describe 'command line execution' do
   subject { AudioAddict::CLI.router }
 
-  commands = [
-    { cmd: "" },
-    { cmd: "status" },
-    { cmd: "status --help" },
-    { cmd: "status --unsafe" },
-    { cmd: "login", kbd: ['n'] },
-    { cmd: "login", kbd: ['y', 'user', 'password'] },
-    { cmd: "set --help" },
-    { cmd: "set dance di" },
-    { cmd: "channels" },
-    { cmd: "channels tran" },
-    { cmd: "channels --help" },
-    { cmd: "now" },
-    { cmd: "now --help" },
-  ]
+  commands = YAML.load_file 'spec/audio_addict/commands.yml'
 
+  # TODO: We probably dont need to test anything against the live server
+  #       so base_uri stuff should move to a helper and executed once only
   let(:mock_api_base) { "http://localhost:3000" }
   let(:live_api_base) { 'https://api.audioaddict.com/v1' }
 
