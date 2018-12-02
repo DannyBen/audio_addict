a5b2021dcceb1ef863208ae9a3239dd91356d6f1
Danny Ben Shitrit <db@dannyben.com>
2018-12-02 20:42:24 +0000

    add log sort

--- lib/audio_addict/commands/log.rb
+++ lib/audio_addict/commands/log.rb
@@ -5,14 +5,14 @@ module AudioAddict
 
       usage "radio log show"
       usage "radio log tail [--lines N]"
-      # usage "radio log sort"
+      usage "radio log sort"
       usage "radio log --help"
 
       option "-l --lines N", "Number of lines to show [default: 5]"
 
       command "show", "Show the entire like log"
       command "tail", "Show the last few rows of the like log"
-      # command "sort", "Sort the log alphabetically and save it"
+      command "sort", "Sort the log alphabetically and save it"
 
       example "radio log show"
       example "radio log tail"
@@ -21,20 +21,31 @@ module AudioAddict
 
       def show_command(args)
         setup
-        puts File.read(Config.like_log)
+        puts File.read(logfile)
       end
 
       def tail_command(args)
         setup
         lines = args['--lines'].to_i
-        puts File.readlines(Config.like_log)[-lines..-1]
+        puts File.readlines(logfile)[-lines..-1]
+      end
+
+      def sort_command(args)
+        setup
+        lines = File.readlines logfile
+        File.write logfile, lines.sort.join
+        say "!txtgrn!Sorted"
       end
 
     private
 
       def setup
         needs :like_log
-        raise Error, "File not found: #{Config.like_log}" unless File.exist? Config.like_log
+        raise Error, "File not found: #{logfile}" unless File.exist? logfile
+      end
+
+      def logfile
+        @logfile ||= Config.like_log
       end
 
     end
